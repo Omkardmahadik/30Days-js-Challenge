9d90f34e98f96c1f4c161e16a3064c6ce732d5c1
Omkar Mahadik <Omkardmahadik@gmail.com>
2024-07-14 18:10:50 +0530

    commit Day1_variablesandDatatypes.js

---
+++ Day1_variablesandDatatypes.js
@@ -0,0 +1,35 @@
+// Day 1: Variable and Data Types
+
+// Activity1:variable declaration
+var no = 5 //var can change the value in scope 
+// console.log(no);
+
+let st="jschallenge" //update the value multiple times
+// console.log(st);
+
+//Activity2: Constant Decalaration
+const boo=true //fix the value you cannot chnage the value
+// console.log(boo);
+
+//Activity3: Data Types
+let ob={
+    name: "omkar",
+    id:true
+}
+let arr=["omkar"]
+
+console.log(typeof no);
+console.log(typeof st);
+console.log(typeof boo);
+console.log(typeof arr);
+
+//Activity4: Reassigning Variables
+let var1 = 5
+var1=10
+console.log(var1);
+
+//Activity5: Understanding Const
+const c=10
+c=20
+console.log(c);
+//error because in const we cannot change the variable multiple times or add the data 
